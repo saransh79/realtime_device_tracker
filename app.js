c24a42a4aadcddf43dea6c2547fd3d3ad8b57f53
saranshgangwar <saransh.gangwar@infinitelocus.com>
2024-09-23 02:15:27 +0530

    added cors

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import cors from "cors";
+
 
 const app = express();
+
+app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server);
 
